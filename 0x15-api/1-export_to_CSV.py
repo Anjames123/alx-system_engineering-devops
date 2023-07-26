ea13656cb1a80e7ad2388c6aa1b3c90973119abb
Anjames123 <ajibadejames19@gmail.com>
2023-07-26 13:27:26 -0700

    tasks

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
+"""Python Script to export data in CSV format"""
 import csv
-import requests
-import sys
+from requests import get
+from sys import argv
 
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+def exportCsv(user):
+    data = get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+        user)).json()
+    name = get('https://jsonplaceholder.typicode.com/users/{}'.format(
+        user)).json().get('username')
+    employee_data = open('{}.csv'.format(user), 'w')
+    cwrite = csv.writer(employee_data, quoting=csv.QUOTE_ALL)
+    for line in data:
+        lined = [line.get('userId'), name,
+                 line.get('completed'), line.get('title')]
+        cwrite.writerow(lined)
+    employee_data.close()
 
-    response = requests.get(url)
-    username = response.json().get('username')
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-
-    with open('{}.csv'.format(employeeId), 'w') as file:
-        for task in tasks:
-            file.write('"{}","{}","{}","{}"\n'
-                       .format(employeeId, username, task.get('completed'),
-                               task.get('title')))
+if __name__ == "__main__":
+    exportCsv(argv[1])
--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python3
-"""Accessing a REST API for todo lists of employees"""
-
+"""
+extending task 0 to export to csv
+"""
 import json
+import re
 import requests
 import sys
 
+# define the API url
+API = "https://jsonplaceholder.typicode.com"
+
+
+def main():
+    """show how many of how many tasks is done by user
+    User id is passed as an argument"""
+    if len(sys.argv) > 1:
+        id = int(sys.argv[1])
+        if re.fullmatch(r"\d+", sys.argv[1]):
+            # get responses
+            emp_res = requests.get('{}/users/{}'.format(API, id)).json()
+            emp_username = emp_res['username']
+            emp_todos = requests.get(
+                '{}/users/{}/todos'.format(API, id)).json()
+            with open('{}.json'.format(id), 'w') as jsonfile:
+                user_data = [{
+                    "task": todo.get('title'),
+                    "completed": todo.get('completed'),
+                    "username": emp_username
+                } for todo in emp_todos]
+
+                user_json = {'{}'.format(id): user_data}
+                json.dump(user_json, jsonfile)
+
 
 if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
-
-    response = requests.get(url)
-    username = response.json().get('username')
-
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-
-    dictionary = {employeeId: []}
-    for task in tasks:
-        dictionary[employeeId].append({
-            "task": task.get('title'),
-            "completed": task.get('completed'),
-            "username": username
-        })
-    with open('{}.json'.format(employeeId), 'w') as filename:
-        json.dump(dictionary, filename)
+    main()
